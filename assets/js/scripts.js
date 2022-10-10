38b3ea9141081be0fc7bd95f381140613ca539e8
Lucas Santos <leos9877@gmail.com>
2022-10-09 23:12:14 -0300

    :rocket: disabled context menu in project

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,13 +3,16 @@ const base = document.getElementById('gallery');
 const letter = () => {
     const characters = 'abcdefghijklmnopqrstuvwxyz'.split("");
 
-    return characters[Math.floor(Math.random() * characters.length)]
+    return characters[ Math.floor(Math.random() * characters.length) ]
 }
 
 const addNewImage = ( element, total ) => {
     for( let count = 1; count <= total; count++ ) {
-        element.innerHTML += `<img src='https://source.unsplash.com/random?${ letter() }=${ count }' />`;
+        element.innerHTML += `<img src='https://source.unsplash.com/random?${ letter() }=${ count }' alt='Imagem ${ count }' />`;
     }
 }
 
 addNewImage( base, 50 )
+
+/* disabled Context Menu on pages */
+document.addEventListener('contextmenu', event => event.preventDefault());
